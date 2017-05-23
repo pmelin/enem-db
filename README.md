fad27a6259bac388ea5953ba869df00e5409815f
Priscilla M <priscillamelin@gmail.com>
2017-05-23 17:14:22 +0100

    Calculates general average

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # enemLoader
+
+## Overview
+
+Node.js project that builds a MongoDB database of Brazilian high schools ranked accordingly to ENEM - a national secondary school exam.   
+
+The data used as input is available in [Brazilian National Institute for Educational Studies and Research "Anísio Teixeira"](http://portal.inep.gov.br/microdados)
--- src/index.js
+++ src/index.js
@@ -2,55 +2,95 @@ var fs = require('fs');
 var csv = require('fast-csv');
 var mongoose = require('mongoose');
 var School = require('../models/School');
+var files = ["PLANILHA_ENEM_ESCOLA_CH_2015.csv", "PLANILHA_ENEM_ESCOLA_CN_2015.csv", "PLANILHA_ENEM_ESCOLA_LC_2015.csv", "PLANILHA_ENEM_ESCOLA_MT_2015.csv", "PLANILHA_ENEM_ESCOLA_RED_2015.csv"];
+var schools = new Map();
 
 mongoose.connect('mongodb://localhost/schoolRanking');
-mongoose.connection.on('open', function() {
-    let parsingOptions = {
-        headers: true,
-        delimiter: ';',
-        objectMode: true
-    };
+mongoose.connection.on('open', async function() {
 
-    var stream = fs.createReadStream("files/test.csv");
+    var insert = true;
+    for (var file of files) {
 
-    var csvStream = csv(parsingOptions).on("data", function(data) {
-        processRow(data);
-    }).on("end", function() {
-        console.log("done");
-    });
+        if (insert) {
+            await processFile(file, processAndInsert);
+            insert = false;
+        } else {
+            await processFile(file, populateAvg);
+        }
+    }
 
-    stream.pipe(csvStream);
+    for (var [code, schoolAvgs] of schools) {
+        var sumAvg = 0;
+
+        for (var avg of schoolAvgs) {
+            sumAvg += avg;
+        }
+
+        var school = await School.findOne({code: code});
+
+        //calculates general average
+        school.average = sumAvg / schoolAvgs.length;
+        await school.save();
+
+    }
 
 });
 
-async function processRow(data) {
-    try {
-        var school = new School({
-            code: data['CÓDIGO DA ENTIDADE'],
-            name: data['NOME DA ENTIDADE'],
-            uf: data['SIGLA DA UF'],
-            municipality: data['NOME MUNICÍPIO'],
-            adminDependency: data['DEPENDÊNCIA ADMINISTRATIVA'],
-            participationRate: replacePontuation(data['TAXA DE PARTICIPAÇÃO']),
-            permanenceRate: data['INDICADOR DE PERMANÊNCIA NA ESCOLA'],
-            teacherTraining: replacePontuation(data['INDICADOR DE FORMAÇÃO DOCENTE']),
-            approvalRate: replacePontuation(data['TAXA DE APROVAÇÃO']),
-            disapprovalRate: replacePontuation(data['TAXA DE REPROVAÇÃO']),
-            abandonmentRate: replacePontuation(data['TAXA DE ABANDONO']),
-            average: replacePontuation(data['MÉDIA ESCOLA'])
+function processFile(file, callback) {
+    return new Promise(function(resolve) {
+        console.log('Processing: ' + file)
+        var parsingOptions = {
+            headers: true,
+            delimiter: ';',
+            objectMode: true
+        };
+        //change to test/ to test with fewer registers
+        var stream = fs.createReadStream("files/" + file);
+
+        var csvStream = csv(parsingOptions).on("data", function(data) {
+            callback(data);
+        }).on("end", function() {
+            resolve();
         });
 
-        await school.save();
-    } catch (err) {
-        console.log('Error processing ' + err);
-    }
+        stream.pipe(csvStream);
+    });
+}
+/*
+Process each school considering csv headers
+*/
+function processRow(data) {
+    return new School({
+        code: data['CÓDIGO DA ENTIDADE'],
+        name: data['NOME DA ENTIDADE'],
+        uf: data['SIGLA DA UF'],
+        municipality: data['NOME MUNICÍPIO'],
+        adminDependency: data['DEPENDÊNCIA ADMINISTRATIVA'],
+        participationRate: data['TAXA DE PARTICIPAÇÃO'],
+        permanenceRate: data['INDICADOR DE PERMANÊNCIA NA ESCOLA'],
+        teacherTraining: data['INDICADOR DE FORMAÇÃO DOCENTE'],
+        approvalRate: data['TAXA DE APROVAÇÃO'],
+        disapprovalRate: data['TAXA DE REPROVAÇÃO'],
+        abandonmentRate: data['TAXA DE ABANDONO'],
+        average: data['MÉDIA ESCOLA']
+    });
 }
 
-function replacePontuation(columnName) {
-    try {
-        columnName = columnName.replace(/,/g, '.');
-        return columnName;
-    } catch (err) {
-        console.log('Error replacing pontuation')
-    }
+/*
+Process the first csv file
+*/
+async function processAndInsert(data) {
+    var school = processRow(data);
+    schools.set(school.code, [school.average]);
+    await school.save();
+}
+
+/*
+Process remaining csv files
+*/
+async function populateAvg(data) {
+    var csvSchool = processRow(data);
+    var avgPerFile = schools.get(csvSchool.code);
+    avgPerFile.push(csvSchool.average);
+    schools.set(csvSchool.code, avgPerFile);
 }
